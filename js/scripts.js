73a67748db0c5bdbd25f1059db002f6a9ce22550
Dani Renner <danijrenner@gmail.com>
2021-01-22 14:28:14 -0800

    incorporate html so that a user can see results

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,44 @@
-let userNumber = 10;
-let count = 0;
-const roboger = [];
-function beepBoop(input){
-  for(i = 0; i <= input; i++) {
-    let iString = i.toString();
-    const digits = iString.split("");
-    let has1 = false;
-    let has2 = false;
-    let has3 = false;
-    for (element of digits) {
-      if (element === "3"){
-        has3 = true;
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const entry = $("input#numberEntry").val();
+    const roboger = [];
+    function beepBoop(input){
+      for(i = 0; i <= input; i++) {
+        let iString = i.toString();
+        const digits = iString.split("");
+        let has1 = false;
+        let has2 = false;
+        let has3 = false;
+        for (element of digits) {
+          if (element === "3"){
+            has3 = true;
+          }
+          else if (element === "2"){
+            has2 = true;
+          }
+          else if (element === "1"){
+          has1 = true;
+          }
+        } //end of for of loop
+        if (has3){
+          roboger.push("Won't you be my neighbor?");
+        }
+        else if (has2){
+          roboger.push("Boop!");
+        }
+        else if (has1){ 
+          roboger.push("Beep!");
+        }
+        else{
+          roboger.push(i);
+        } 
       }
-      else if (element === "2"){
-        has2 = true;
-      }
-      else if (element === "1"){
-      has1 = true;
-      }
-    } //end of for of loop
-    if (has3){
-      roboger.push("Won't you be my neighbor?");
+      //end of for i loop
+      return roboger.toString();
     }
-    else if (has2){
-      roboger.push("Boop!");
-    }
-    else if (has1){ 
-      roboger.push("Beep!");
-    }
-    else{
-      roboger.push(i);
-    } 
-  }
-  //end of for i loop
-  return roboger.toString();
-}
+    $(".output").text(beepBoop(entry));
+  });
+});
 
 
