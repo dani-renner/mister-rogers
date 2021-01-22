0df5d6767a308d393e3d0db22478f129d725a8c1
Dani Renner <danijrenner@gmail.com>
2021-01-22 10:27:27 -0800

    function beepBoop can check for 1s,2s,&3s and print string of just numbers if none

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,23 @@ function beepBoop(number){
   number=number.toString();
   const digits =[];
   digits.push(number.split(""));
-  
-  while (count<=number) {
-    roboger.push(count);
-    count++;
-  };
-return roboger.toString();
+  for (element of digits){
+    if (element = "3"){
+      return "Won't you be my neighbor?";
+    }
+    else if (element = "2"){
+      return "Boop";
+    }
+    else if (element ="1"){
+      return "Beep!";
+    }
+    else {
+      while (count<=number) {
+        roboger.push(count);
+        count++;
+      }
+      return roboger.toString();
+    }
+  }
 }
 
