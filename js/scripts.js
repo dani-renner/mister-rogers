7685a00f472caff9154ecbbf77e62e468079e323
Dani Renner <danijrenner@gmail.com>
2021-01-22 09:43:57 -0800

    function will turn number into a string, then array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 let userNumber = 10;
 let count = 0;
 const roboger = [];
-function beepBoop(){
-  while (count<=userNumber) {
+function beepBoop(number){
+  number=number.toString();
+  const digits =[];
+  digits.push(number.split(""));
+  
+  while (count<=number) {
     roboger.push(count);
     count++;
   };
