55b5df5dfe2747c66efcf6de03d58550f1ec2ef2
Dani Renner <danijrenner@gmail.com>
2021-01-22 13:49:53 -0800

    fix an issue where I was pushing an array into an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,12 @@ let userNumber = 10;
 let count = 0;
 const roboger = [];
 function beepBoop(input){
-  for(i=0; i<=input; i++) {
-    let iString=i.toString();
-    const digits =[];
-    digits.push(iString.split(""));
+  for(i = 0; i <= input; i++) {
+    let iString = i.toString();
+    const digits = iString.split("");
     let has1 = false;
     let has2 = false;
     let has3 = false;
-
     for (element of digits) {
       if (element === "3"){
         has3 = true;
