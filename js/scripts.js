6e96b1bde4211892f92c5b82216015f977a6c452
Dani Renner <danijrenner@gmail.com>
2021-01-22 13:36:23 -0800

    reworked function to a loop inside a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
 let userNumber = 10;
 let count = 0;
 const roboger = [];
-function beepBoop(userNumber){
-  number=userNumber.toString();
-  const digits =[];
-  digits.push(number.split(""));
-  digits.forEach(function(element){
-      if (element==="3"){
-        roboger.push("Won't you be my neighbor?");
+function beepBoop(input){
+  for(i=0; i<=input; i++) {
+    let iString=i.toString();
+    const digits =[];
+    digits.push(iString.split(""));
+    let has1 = false;
+    let has2 = false;
+    let has3 = false;
+
+    for (element of digits) {
+      if (element === "3"){
+        has3 = true;
       }
-      else if (element==="2"){
-        roboger.push("Boop");
+      else if (element === "2"){
+        has2 = true;
       }
-      else if (element==="1"){
-        roboger.push("Beep!");
+      else if (element === "1"){
+      has1 = true;
       }
-      else {
-        roboger.push(count);
-      }
-      count++;
-    });
+    } //end of for of loop
+    if (has3){
+      roboger.push("Won't you be my neighbor?");
+    }
+    else if (has2){
+      roboger.push("Boop!");
+    }
+    else if (has1){ 
+      roboger.push("Beep!");
+    }
+    else{
+      roboger.push(i);
+    } 
+  }
+  //end of for i loop
   return roboger.toString();
 }
+
 
